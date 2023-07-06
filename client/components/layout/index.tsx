1900c87476534dd21e1f2ceec328411955104ed1
Tyler Candee <tyler@candeegenerations.com>
2023-07-06 11:40:01 -0400

    fix: updates pwa icons

--- client/components/layout/index.tsx
+++ client/components/layout/index.tsx
@@ -7,10 +7,12 @@ const Layout = ({children}) => {
     <>
       <TopPromo />
 
-      <div className="container max-w-screen-xl mx-auto p-5 md:p-16 lg:p-10">
-        {children}
+      <div className="bg-white">
+        <div className="container max-w-screen-xl mx-auto p-5 md:p-16 lg:p-10">
+          {children}
 
-        <Footer />
+          <Footer />
+        </div>
       </div>
     </>
   )
