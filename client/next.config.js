f79c40c78def6199e99fd77b7aa4e7a50744ef5d
Tyler Candee <tyler@candeegenerations.com>
2024-05-10 11:46:33 -0400

    chore: build fix

--- client/next.config.js
+++ client/next.config.js
@@ -1,15 +1,16 @@
+/* eslint-disable no-undef */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      'localhost',
-      'readthroughyourbible.com',
-      'lh3.googleusercontent.com',
-    ],
+    domains: ['localhost', 'readthroughyourbible.com', 'lh3.googleusercontent.com'],
     unoptimized: true,
   },
   env: {
     siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
+  },
+  experimental: {
+    workerThreads: false,
+    cpus: 1,
   },
 }
 
